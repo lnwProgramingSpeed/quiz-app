ce4a6c5522c75e219e669a0c80dfd69190649c9e
Phawat Phuengdecha <pawat.pooh@gmail.com>
2023-07-31 14:40:17 +0700

    service api and add more img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { question } from './quiz_data';
+import { Component, inject } from '@angular/core';
 import { Choice } from './question';
+import { QuizService } from './quiz.service';
+import { Question } from './question';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +9,63 @@ import { Choice } from './question';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  question = question;
+  quizService: QuizService = inject(QuizService);
+
+  question: Question[];
   currentIndex = 0;
   score = 0;
+  isEnd = false;
+  click = new Audio();
+  bg = new Audio();
+
+  winner='';
+
+  constructor() {
+    this.question = this.quizService.getQuizData();
+    this.click.src = '../assets/audio/explosion.wav';
+    this.onClickRestart();
+  }
 
   onClickChoice(Choice: Choice) {
-    if (this.currentIndex < this.question.length - 1) {
+    this.clickSound();
+
+    if (this.currentIndex < this.question.length) {
       console.log(this.currentIndex);
       console.log(this.question.length);
       this.currentIndex++;
     }
+    if (this.currentIndex == this.question.length) {
+      this.isEnd = true;
+      this.checkScore(this.score);
+    }
     return Choice.isAnswer ? this.score += 1 : this.score += 0;
   }
+
+  private checkScore(score: number){
+    if(score == this.question.length && score != this.question.length){
+      this.winner = '../assets/images/smart.jpg'
+    }else if(score > this.question.length/2){
+      this.winner = '../assets/images/ohMyGwab.jpg'
+    } else {
+      this.winner = '../assets/images/joker.jpg'
+    }
+  }
+
+  private clickSound() {
+    this.click.load();
+    this.click.addEventListener('canplaythrough', () => {
+      this.click.play();
+    });
+  }
+
+  onClickRestart(){
+    this.score = 0;
+    this.restartGame();
+  }
+
+  private restartGame() {
+    this.currentIndex = 0;
+    this.isEnd = false;
+  }
 }
+
